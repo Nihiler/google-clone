407f798c654c6bc497c79440d0695f8df0594f87
jpr <julioperezrdlf@gmail.com>
2022-01-06 00:41:40 +0100

    i got no idea of this  bug.

--- google-clone/src/components/Search.js
+++ google-clone/src/components/Search.js
@@ -4,15 +4,24 @@ import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button} from "@material-ui/core";
 import {useHistory} from "react-router-dom";
+import { useStateValue } from '../StateProvider';
+// import { ActionTypes} from "..reducer";
+import { actionTypes } from '../reducer';
 
 function Search({ hideButtons = false }) {
+    const [{}, dispatch] = useStateValue();
 
-    const[input, setInput] = useState ('');
+    const [input, setInput] = useState ("");
     const history = useHistory();
 
     const search = (e) => {
         e.preventDefault();
         console.log("Search Button -> Hit; Data: ", input)
+
+        dispatch({
+            type: actionTypes.SET_SEARCH_TERM,
+            term: input
+        })
         //do something with input. come back and fix
         history.push('/search');
     }
@@ -41,7 +50,7 @@ function Search({ hideButtons = false }) {
                     <Button className='search__buttonsHidden'
                     type= "submit" onClick={search}
                      variant ="outlined">Google Search</Button>
-                     
+
                     <Button className='search__buttonsHidden'
                     variant ="outlined">I'm feeling lucky</Button>
                     </div>
